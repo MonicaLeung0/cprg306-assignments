ff8cf47c96a5847924e3b0a1a624f42079b87ca9
MonicaLeung0 <manki.leung@edu.sait.ca>
2025-09-16 14:49:16 -0600

    edit week-3 assignment

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -3,8 +3,7 @@ export default function Item({ name, quantity, category })
     return (
         <div className="border border-sky-500 bg-sky-800 w-full max-w-xs m-4 p-2 rounded-lg  a">
             <h3 className="text-xl font-bold">{name}</h3>
-            <p className="text-sm">Quantity: {quantity}</p>
-            <p className="text-sm" >Category: {category}</p>
+            <p className="text-sm">Buy {quantity} in {category} </p>
         </div>
     );
 }
